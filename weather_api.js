36378bc1e8ef5b2e1990c292f09b90baada1b983
Isaac Harris <nixrevolution@gmail.com>
2021-02-10 11:51:05 +0000

    Added a button to call api

--- weather_api.js
+++ weather_api.js
@@ -1,3 +1,7 @@
+document.getElementById("scan").onclick = function() {weatherFunction()};
+
+function weatherFunction() {
+
 const ul = document.getElementById("weather");
 const url = "http://api.openweathermap.org/data/2.5/weather?lat=51.5074&lon=0.1278&appid=7edd3815d4262f09773bd7e52ac2512c";
 let weather = "";
@@ -13,3 +17,4 @@ fetch(url)
 });
 setTimeout(() => { document.getElementById("weather").classList.remove("loader"); ul.innerHTML = weather; }, 200);
 
+}
